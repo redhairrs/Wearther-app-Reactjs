90a4e33a9b2253f3fe3b9b6eb3c6faf0325eb267
Rishabh Sharma <86844932+redhairrs@users.noreply.github.com>
2023-02-16 19:49:56 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,12 @@ import ChooseStateComponents from './ChooseState';
 import HumidityComponents from './HUMIDITY';
 import LeftComponents from './Left';
 
+import { UseWeatherAppContext } from '../Context/Context';
+
 export default function HomeComponents() {
     return (
         <>
-            <div className='homeWrap'>
+            <div className='homeWrap' style={{backgroundImage:`url('https://source.unsplash.com/1600x900/?${city.city}')`}}>
                 <div className='weatherSection'>
                     <LeftComponents />
                     <div className='rightSide'>
